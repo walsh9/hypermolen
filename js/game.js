d1a658475ac950261bbd4859452fa00360ca49cc
walsh9 <walsh9@gmail.com>
2016-04-11 23:55:23 -0400

    Updates

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-Vector2d = {
+var Vector2d = {
   init: function(pos) {
     this.x = pos.x;
     this.y = pos.y;
@@ -20,10 +20,14 @@ Vector2d = {
     var dirY = deltaY / distance;
     return Vector2d.new({x: dirX , y: dirY});
   },
+  move: function(direction, speed) {
+    this.x += direction.x * speed;
+    this.y += direction.y * speed;
+  },
   moveTowards: function(pos, speed) {
-    var dir = this.directionTo(pos);
-    this.x += dir.x * speed;
-    this.y += dir.y * speed;
+    var direction = this.directionTo(pos);
+    this.x += direction.x * speed;
+    this.y += direction.y * speed;
   },
   isNear: function(pos) {
     return(this.distanceTo(pos) < 1)
@@ -32,7 +36,7 @@ Vector2d = {
 
 Math.mod = function(x, y) {
   return ((x % y) + y) % y;
-},
+}
 
 HyperMolen = function(canvas) {
   var CIRCLE = Math.PI * 2;
@@ -45,7 +49,6 @@ HyperMolen = function(canvas) {
   var MID_X = W / 2;
   var greenLeft  = MID_X + 10;
   var greenRight = MID_X - 10;
-  var greenPerspective = 6;
 
   var createArms = function(numArms, width) {
     var start, end;
@@ -58,10 +61,39 @@ HyperMolen = function(canvas) {
     return arms;
   };
 
+  var Ball = {
+    init: function(collection) {
+      this.color = randomColor();
+      this.radius = 2;
+      this.pos = Vector2d.new({x: Math.random() * 64, y: 64});
+      this.speed = Math.random() * 6 + 18;
+      this.direction = this.pos.directionTo({x: 32, y: 33});
+      this.collection = collection;
+      this.index = collection.push(this) - 1;
+      return this;
+    },
+    update: function(timePassed) {
+        var speed = this.speed * timePassed;
+        this.pos.move(this.direction, speed);
+        if (this.pos.isNear({x: 32, y: 33})) {
+          if (checkBlocking()) {
+            this.direction.y = -(this.direction.y);
+            // this.targets.shift();
+            // this.targets.push({x: Math.random() * 64, y: 68}); 
+          } else {
+            this.destroy();
+          }
+        }
+    },
+    destroy: function() {      
+        delete this.collection[this.index];
+    }
+  };
+
   var molen = {};
   molen.angle = 0;//CIRCLE / 4 - CIRCLE * 0.125;
   molen.pos = {x: MID_X, y:10};
-  molen.armRadius = 25;
+  molen.armRadius = 20;
   molen.armCount = 4;
   molen.armWidth = 0.125;
   molen.arms = createArms(molen.armCount, molen.armWidth);
@@ -69,8 +101,12 @@ HyperMolen = function(canvas) {
   var windSpeed = -3;
   var balls = [];
 
+  var randomColor = function() {
+    var colors = ["#00ffff","#ffff00","#ff9999","ff3333","ff00ff","00ff00"];
+    return colors[Math.floor(Math.random() * colors.length)]
+  };
 
-  var checkHit = function(offset) {
+  var checkBlocking = function(offset) {
     var armStart, armEnd
     offset = offset || CIRCLE * 0.25;
     return molen.arms.some(function(arm) {
@@ -83,41 +119,9 @@ HyperMolen = function(canvas) {
       }
     });
   };
-  var randomColor = function() {
-    var colors = ["#00ffff","#ffff00","#ff9999","ff3333","ff00ff","00ff00"];
-    return colors[Math.floor(Math.random() * colors.length)]
-  }
-
-  var ball = {
-    init: function() {
-      this.color = randomColor();
-      this.radius = 2;
-      this.pos = Vector2d.new({x: Math.random() * 64, y: 64});
-      this.speed = Math.random() * 6 + 18;
-      this.targets = [Vector2d.new({x: 32, y: 33})];
-      this.index = balls.push(this) - 1;
-      return this;
-    },
-    update: function(timePassed) {
-      if (this.targets.length > 0) {
-        var speed = this.speed * timePassed;
-        this.pos.moveTowards(this.targets[0], speed);
-        if (this.pos.isNear(this.targets[0])) {
-          if (checkHit()) {
-            this.targets.shift();
-            this.targets.push({x: Math.random() * 64, y: 68}); 
-          } else {
-            this.targets.shift();
-          }
-        }
-      } else {
-        delete balls[this.index];
-      }
-    }
-  };
 
   var addBall = function() {
-    Object.create(ball).init();
+    Object.create(Ball).init(balls);
   };
 
   /* Keyboard handling */
@@ -130,9 +134,6 @@ HyperMolen = function(canvas) {
 
 
   var update = function(timePassed) {
-    if (isPaused) {
-      return;
-    }
     var delta = timePassed * 2;
     molen.angle = Math.mod(molen.angle + windSpeed * timePassed, CIRCLE);
     if (39 in keysDown) { // right
@@ -150,7 +151,6 @@ HyperMolen = function(canvas) {
     });
   };
 
-
   var drawPieSlice = function(ctx, x, y, radius, startAngle, endAngle) {
     ctx.save();
     ctx.setTransform(1, 0, 0, 1, 0, 0);
@@ -165,6 +165,15 @@ HyperMolen = function(canvas) {
   var draw = function() {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, W, H);
+
+    //draw sky
+    ctx.fillStyle = "#0000ff";
+    var grd=ctx.createLinearGradient(0,0,0,64);
+    grd.addColorStop(0,"#000000");
+    grd.addColorStop(1,"#ff88ff");
+    ctx.fillStyle = grd;  
+    ctx.fillRect(0, 0, W, 35);
+
     //draw body
     ctx.beginPath();
     ctx.fillStyle = "#D2B48C";
@@ -193,8 +202,8 @@ HyperMolen = function(canvas) {
     ctx.fillStyle = "#007700";
     ctx.moveTo(greenLeft, 35);
     ctx.lineTo(greenRight, 35);
-    ctx.lineTo(greenRight - greenPerspective, 64);
-    ctx.lineTo(greenLeft + greenPerspective, 64);
+    ctx.lineTo(0, 64);
+    ctx.lineTo(64, 64);
     ctx.fill();
 
     //draw heart
@@ -213,10 +222,41 @@ HyperMolen = function(canvas) {
       //              CIRCLE * arm.start + molen.angle, 
       //              CIRCLE * arm.end + molen.angle);
       ctx.rotate(1 / molen.armCount * CIRCLE)
-      ctx.fillStyle = "#ffffff";
-      ctx.fillRect(molen.heartRadius, 0, molen.armRadius - 5, (arm.end - arm.start) * 130)
-      ctx.fillStyle = "#ff6600";
-      ctx.fillRect(0, 0, molen.armRadius + molen.heartRadius - 5, (arm.end - arm.start) * 20)
+      var bladeLength = molen.armRadius;
+      var bladeAngle = (arm.end - arm.start) * CIRCLE;
+      var bladeChord = Math.sqrt(2 * Math.pow(bladeLength, 2) - 2 * Math.pow(bladeLength, 2) * Math.cos(bladeAngle));
+      var otherAngle = (CIRCLE/2 - bladeAngle)/2
+      var bladeHeight = Math.sin(otherAngle) * bladeChord;
+      var fakeHeight = (arm.end - arm.start) * 130;
+      var frameThickness = bladeLength/20;
+      var armThickness = frameThickness * 2;
+
+      var sailColor = "#ffffff";
+      var frameColor = "#ff6600";
+      var lengthSectors = 6;
+      var heightSectors = 3;
+
+      ctx.fillStyle = frameColor;
+      ctx.fillRect(molen.heartRadius, 0, bladeLength, bladeHeight);
+      // ctx.fillRect(molen.heartRadius, 0, molen.armRadius - 5, (arm.end - arm.start) * 130);
+
+      ctx.fillStyle = sailColor;
+      ctx.fillRect(molen.heartRadius + frameThickness, frameThickness, bladeLength - frameThickness * 2, bladeHeight - frameThickness * 2);
+      //ctx.fillRect(molen.heartRadius + 1, 0 + 1, molen.armRadius - 5 -2, (arm.end - arm.start) * 130 - 2);
+
+      ctx.fillStyle = frameColor;
+      ctx.fillRect(0, 0, molen.heartRadius + bladeLength, armThickness);
+
+      for (var line = 1; line <= lengthSectors; line++) {
+         var x = (bladeLength - frameThickness * 2) * line / lengthSectors;
+         ctx.fillRect(molen.heartRadius + x, 0, frameThickness, bladeHeight)
+      }
+
+      for (var line = 1; line <= heightSectors; line++) {
+        var y = (bladeHeight - frameThickness * 2) * line / heightSectors;
+        ctx.fillRect(molen.heartRadius, y + frameThickness, bladeLength, frameThickness)
+      }
+
     });
     ctx.setTransform(1, 0, 0, 1, 0, 0)
 
@@ -231,11 +271,12 @@ HyperMolen = function(canvas) {
 
   };
 
-
-
-  var runTime = 0;
-  var timeStep = 1 / 30; 
-  var currentTime = Date.now();
+  var runTime, timeStep, currentTime
+  var initLoop = function() {
+    runTime = 0;
+    timeStep = 1 / 30; 
+    currentTime = Date.now();
+  }
   var mainLoop = function () {
           var newTime = Date.now();
           var frameTime = newTime - currentTime;
@@ -247,23 +288,24 @@ HyperMolen = function(canvas) {
               runTime += delta;
           }
       draw();
-      if (!gameOver) {
+      if (!gameOver && !isPaused) {
           requestAnimationFrame(mainLoop, canvas);
       }
       else {
-          drawGameOver(ctx);
-          window.addEventListener("keydown", function(e) {
-              if(e.keyCode === 82) {
-                  location.reload();
-              }
-          }, false);
+          // drawGameOver(ctx);
+          // window.addEventListener("keydown", function(e) {
+          //     if(e.keyCode === 82) {
+          //         location.reload();
+          //     }
+          // }, false);
       }
   };
 
   window.addEventListener('blur', function() {isPaused = true;})
-  window.addEventListener('focus', function() {isPaused = false;})
+  window.addEventListener('focus', function() {isPaused = false; initLoop(); mainLoop();})
 
   // initGame();
   // initLevel(1);
+  initLoop();
   mainLoop();
 }(document.querySelector('#canvas'));
